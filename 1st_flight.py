db1cf4c0dc1a00bb278947da2d7a734ae0166ac3
Andrei Korigodskii <akorigod@gmail.com>
2021-11-28 07:04:04 +0300

    Calculate splines and navigate by them

--- 1st_flight.py
+++ 1st_flight.py
@@ -5,6 +5,7 @@ from clover import srv
 from std_srvs.srv import Trigger
 from mavros_msgs.srv import ParamSet
 from mavros_msgs.msg import ParamValue
+from spline import spline_drone, enu_vector
 
 rospy.init_node('flight')
 
@@ -55,22 +56,44 @@ with open('/home/user/drone-games/tasks/cargo/2/gps_spline.pts') as file:
         l = line.split()
         crd_list.append((float(l[0]), float(l[1]), float(l[2])))
 
+home_position_0 = get_telemetry[0](frame_id='map')
+home_position_1 = get_telemetry[1](frame_id='map')
+home_position_2 = get_telemetry[2](frame_id='map')
+
+# Координаты первой точки траектории в локальных системах каждого из дронов
+first_point_coords = [
+    enu_vector([home_position_0.lat, home_position_0.lon, home_position_0.alt], crd_list[0]),
+    enu_vector([home_position_1.lat, home_position_1.lon, home_position_1.alt], crd_list[0]),
+    enu_vector([home_position_2.lat, home_position_2.lon, home_position_2.alt], crd_list[0])
+]
 
 navigate[0](x=0, y=0, z=5, speed=1, frame_id='body', auto_arm=True)
 rospy.sleep(5)
 
 i = 0
-for crd in crd_list:
+for t, crd in [(a/100.0, (
+        spline_drone(a/100.0, 0),
+        spline_drone(a/100.0, 1),
+        spline_drone(a/100.0, 2)
+        )) for a in range(100)
+    ]:  # При 100 точках расстоние между ними будет около 10 м
+
     i+=1
     print(f"flying to {crd}")
-    navigate_global[0](lat=crd[0], lon=crd[1], z=crd[2], speed=SPEED, frame_id='map')
+    navigate[0](x=crd[0][0]+first_point_coords[0][0], y=crd[0][1]+first_point_coords[0][1], z=crd[0][2]+first_point_coords[0][2], speed=SPEED, frame_id='map')
+    navigate[1](x=crd[1][0]+first_point_coords[1][0], y=crd[1][1]+first_point_coords[1][1], z=crd[1][2]+first_point_coords[1][2], speed=SPEED, frame_id='map')
+    navigate[2](x=crd[2][0]+first_point_coords[2][0], y=crd[2][1]+first_point_coords[2][1], z=crd[2][2]+first_point_coords[2][2], speed=SPEED, frame_id='map')
     wait_arrival()
-    if i == 7:
-        client.call_async('dropCargo', vehicle_name).join()
-        set_rate_k(0, 0.3)
+    # Тут надо будет сравнивать текущее t с t сброса груза
+    # Найти t сброса груза можно найдя в gps_spline.pts точку с lat, lon
+    # соответствующими точке сброса, и взяв ее t (той же строки) из spline_t.txt
+
+    # if i == 7:
+    #     client.call_async('dropCargo', vehicle_name).join()
+    #     set_rate_k(0, 0.3)
 
 
-navigate_global[0](lat=crd_list[0][0], lon=crd_list[0][1], z=10, speed=SPEED, frame_id='map')
-wait_arrival()
+# navigate_global[0](lat=crd_list[0][0], lon=crd_list[0][1], z=10, speed=SPEED, frame_id='map')
+# wait_arrival()
 
 land[0]()
---
+++ spline.py
@@ -0,0 +1,52 @@
+import math
+from scipy.interpolate import interp1d
+
+
+D_VALUE = 2  # Параметр D из задания - расстояние от дрона до центра масс группы
+DRONE_COUNT = 3
+
+#!!!
+# в Mavros при выдаче в топик global_position/global высота задана в элипсоиде WGS-84 и не равна высоте над уровнем моря (AMSL)
+# см. https://wiki.ros.org/mavros#mavros.2FPlugins.Avoiding_Pitfalls_Related_to_Ellipsoid_Height_and_Height_Above_Mean_Sea_Level
+#!!!
+# преобразование дельты gps в дельту xyz по простейшей формуле (из интернета)
+def enu_vector(g1, g2):
+    n = g2[0] - g1[0]
+    e = g2[1] - g1[1]
+    u = g2[2] - g1[2]
+    refLat = (g1[0] + g2[0]) / 2
+    nm = n * 333400 / 3  # deltaNorth * 40008000 / 360
+    em = e * 1001879 * math.cos(
+        math.radians(refLat)) / 9  # deltaEast * 40075160 *cos(refLatitude) / 360
+    return [em, nm, u]
+
+
+with open('/home/user/drone-games/tasks/cargo/2/gps_spline.pts') as file:
+    gps_coords = [[float(a) for a in line.rstrip().split()] for line in file]
+
+with open('/home/user/drone-games/tasks/cargo/2/spline_t.txt') as file:
+    gps_coords_t = [float(line.rstrip()) for line in file if line.rstrip()]
+
+local_coords = [enu_vector(gps_coords[0], g2) for g2 in gps_coords]
+
+splines_xyz = (interp1d(gps_coords_t, [xyz[0] for xyz in local_coords], kind='cubic'),
+               interp1d(gps_coords_t, [xyz[1] for xyz in local_coords], kind='cubic'),
+               interp1d(gps_coords_t, [xyz[2] for xyz in local_coords], kind='cubic'))
+
+
+# Изначальный сплайн (по точкам из условия)
+def spline(t):
+    return (float(splines_xyz[0](t)), float(splines_xyz[1](t)), float(splines_xyz[2](t)))
+
+
+# Возвращает точку траектории на момент времени t (t лежит от 0 до 1 для всего полета,
+# от начальной точки до конечной) для дрона drone_num. Дроны расположены треугольником
+# вокруг точки изначального (построенного по точкам из условия задачи) сплайна, каждый
+# на расстоянии D_VALUE от этой точки. Траектория в локальной системе относительно
+# своей первой точки!
+def spline_drone(t, drone_num):
+    return (
+        float(splines_xyz[0](t) + D_VALUE * math.cos(2 * math.pi / DRONE_COUNT * drone_num)), 
+        float(splines_xyz[1](t) + D_VALUE * math.sin(2 * math.pi / DRONE_COUNT * drone_num)), 
+        float(splines_xyz[2](t))
+    )
